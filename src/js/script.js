3fc9f5d2a5c63f7aba32f3842437f8f4cbb6b073
onndes <vladvyljotnikov@gmail.com>
2021-05-27 08:05:03 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,42 @@
+// slick-slider check-comforts
+$(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    //     fade: true,
+    asNavFor: ".slider-nav",
+});
+$(".slider-nav").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+//     variableWidth: true,
+});
 
+// ====================================
 function initMobile() {
     console.log("is-mobile");
-
 }
-
 
 function initDesktop() {
     console.log("is-desktop");
- }
-
-
-
-
+}
 
 ssm.addState({
-    id: 'tablet',
-    query: '(max-width: 768px)',
-    onEnter: function(){
+    id: "tablet",
+    query: "(max-width: 768px)",
+    onEnter: function () {
         initMobile();
-    }
+    },
 });
 
 ssm.addState({
-    id: 'desktop',
-    query: '(min-width: 768px)',
-    onEnter: function(){
+    id: "desktop",
+    query: "(min-width: 768px)",
+    onEnter: function () {
         initDesktop();
-    }
+    },
 });
